31d8924f66c8d6e54205dc6cd7a1e1bcb4b1fea9
ncastellani <me@ncastellani.com>
2023-07-29 19:23:00 -0300

    implemented some converters

---
+++ converters.go
@@ -0,0 +1,39 @@
+package partida
+
+import "gopkg.in/guregu/null.v4"
+
+func ExtractNullError(err error) (v null.String) {
+	if err != nil {
+		v = null.NewString(err.Error(), true)
+	}
+	return v
+}
+
+func ExtractNullInt(data interface{}) (v null.Int) {
+	switch data.(type) {
+	case float64:
+		v = null.NewInt(int64(data.(float64)), true)
+	}
+	return v
+}
+
+func ExtractNullString(data interface{}) (v null.String) {
+	switch data.(type) {
+	case string:
+		v = null.NewString(data.(string), true)
+	}
+	return v
+}
+
+func ExtractStringArray(data interface{}) (v []string) {
+	switch data.(type) {
+	case []interface{}:
+		for _, e := range data.([]interface{}) {
+			switch data.(type) {
+			case string:
+				v = append(v, e.(string))
+			}
+		}
+	}
+	return v
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/ncastellani/partida
 go 1.20
 
 require github.com/aws/aws-lambda-go v1.37.0
+
+require gopkg.in/guregu/null.v4 v4.0.0
